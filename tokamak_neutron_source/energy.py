ce5ee798f3774c6ff4d5421a22fbd36fe78a6013
Matti <matti.coleman@gmail.com>
2025-10-02 09:29:48 +0200

    plot tests :/

--- tokamak_neutron_source/energy.py
+++ tokamak_neutron_source/energy.py
@@ -38,41 +38,50 @@ class BallabioEnergySpectrum:
 
     omega_0: float  # [keV]
 
-    """\Delta E_{th}"""
-    energy_shift: BallabioCoefficients
+    """\Delta E_{th} coefficients"""
+    energy_shift_coeffs: BallabioCoefficients
 
-    """\delta_{\omega}"""
-    width_correction: BallabioCoefficients
+    """\delta_{\omega} coefficients"""
+    width_correction_coeffs: BallabioCoefficients
+
+    def energy_shift(self, temp_kev: float | npt.NDArray) -> float | npt.NDArray:
+        """
+        Calculate the energy shift \Delta E_{th} at a given ion temperature.
+        """
+        return ballabio_fit(temp_kev, self.energy_shift_coeffs)
+    
+    def width_correction(self, temp_kev: float | npt.NDArray) -> float | npt.NDArray:
+        """
+        Calculate the width correction \delta_{\omega} at a given ion temperature.
+        """
+        return ballabio_fit(temp_kev, self.width_correction_coeffs)
 
     def mean_energy(self, temp_kev: float | npt.NDArray) -> float | npt.NDArray:
         """
         Calculate the mean neutron energy at a given ion temperature (first moment: mu).
         """
-        return self.energy_0 + ballabio_fit(temp_kev, self.energy_shift)
+        return self.energy_0 + self.energy_shift(temp_kev)
 
     def std_deviation(self, temp_kev: float | npt.NDArray) -> float | npt.NDArray:
         """
         Calculate the standard deviation of the neutron energy spectrum at a given ion
         temperature (second moment: sigma)
         """
-        delta_omega = ballabio_fit(temp_kev, self.width_correction)
-
         # Full width at half maximum
-        w_12 = self.omega_0 * (1 + delta_omega) * np.sqrt(temp_kev)
-
+        w_12 = self.omega_0 * (1 + self.width_correction(temp_kev)) * np.sqrt(temp_kev)
         return w_12 / TWO_SQRT_2LN2
 
 
 BALLABIO_DT_NEUTRON = BallabioEnergySpectrum(
     energy_0=14021.0,
     omega_0=177.259,
-    energy_shift=BallabioCoefficients(
+    energy_shift_coeffs=BallabioCoefficients(
         a1=5.30509,
         a2=2.4736e-3,
         a3=1.84,
         a4=1.3818,
     ),
-    width_correction=BallabioCoefficients(
+    width_correction_coeffs=BallabioCoefficients(
         a1=5.1068e-4,
         a2=7.6223e-3,
         a3=1.78,
@@ -83,13 +92,13 @@ BALLABIO_DT_NEUTRON = BallabioEnergySpectrum(
 BALLABIO_DD_NEUTRON = BallabioEnergySpectrum(
     energy_0=2.4495e3,
     omega_0=82.542,
-    energy_shift=BallabioCoefficients(
+    energy_shift_coeffs=BallabioCoefficients(
         a1=4.69515,
         a2=-0.040729,
         a3=0.47,
         a4=0.81844,
     ),
-    width_correction=BallabioCoefficients(
+    width_correction_coeffs=BallabioCoefficients(
         a1=1.7013e-3,
         a2=0.16888,
         a3=0.49,
@@ -111,30 +120,3 @@ def ballabio_fit(
         data.a1 / (1 + data.a2 * temp_kev**data.a3) * temp_kev ** (2 / 3)
         + data.a4 * temp_kev
     )
-
-
-if __name__ == "__main__":
-
-    def normal_pdf(x, mu, sigma):
-        return (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(
-            -((x - mu) ** 2) / (2 * sigma**2)
-        )
-
-    import matplotlib.pyplot as plt
-
-    s = BALLABIO_DT_NEUTRON
-
-    ti = 20.0
-    mu = s.mean_energy(ti)
-    sigma = s.std_deviation(ti)
-
-    spectrum = np.random.normal(mu, sigma, 10000)
-
-    energy = np.linspace(mu - 8 * sigma, mu + 8 * sigma, 1000)
-    prob = normal_pdf(energy, mu, sigma)
-
-    f, ax = plt.subplots()
-    ax.semilogy(energy, prob)
-    ax.set_xlim([12e3, 17e3])
-    ax.set_ylim([10e-11, 10e-3])
-    plt.show()
