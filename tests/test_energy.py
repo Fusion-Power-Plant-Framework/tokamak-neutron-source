045b6847dc3992cdce9060a86a89d80e47440907
Matti <matti.coleman@gmail.com>
2025-10-06 09:45:16 +0200

    add test

--- tests/test_energy.py
+++ tests/test_energy.py
@@ -1,22 +1,17 @@
 # SPDX-FileCopyrightText: 2024-present Tokamak Neutron Source Maintainers
 #
 # SPDX-License-Identifier: LGPL-2.1-or-later
-import matplotlib.pyplot as plt
+
 import numpy as np
 import pytest
 
-from tokamak_neutron_source.energy import (
-    _gaussian_energy_spectrum,
-    _modified_gaussian_energy_spectrum,
-    energy_spectrum,
-)
+
+from tokamak_neutron_source.energy import energy_spectrum
 from tokamak_neutron_source.energy_data import (
     BALLABIO_DD_NEUTRON,
     BALLABIO_DT_NEUTRON,
-    TT_N_SPECTRUM,
 )
 from tokamak_neutron_source.reactions import Reactions
-
 
 class TestEnergyShift:
     """Values graphically determined from Ballabio et al., 1998"""
@@ -33,3 +28,9 @@ class TestEnergyShift:
     def test_energy_shift(self, reaction_spectrum, temperature, expected):
         e_shift = reaction_spectrum.energy_shift(temperature)
         assert np.isclose(e_shift, expected, rtol=1e-2, atol=0.0)
+
+@pytest.mark.parametrize("reaction", [Reactions.D_D, Reactions.D_T, Reactions.T_T])
+def test_normalised_spectra(reaction):
+    energy, prob = energy_spectrum(20.0, reaction)
+
+    assert np.isclose(np.trapz(prob, energy), 1.0)
