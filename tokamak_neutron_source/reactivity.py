551240c46b92dd46799b93a58b41d09df787fa58
Matti <matti.coleman@gmail.com>
2025-10-07 15:37:45 +0200

    missing word

--- tokamak_neutron_source/reactivity.py
+++ tokamak_neutron_source/reactivity.py
@@ -79,7 +79,7 @@ def density_weighted_reactivity(
         case Reactions.D_T:
             n1_n2 = density_d * density_t
         case Reactions.T_T:
-            # Here the Kronecker term (1/1+1) because they are the same
+            # Here the Kronecker term is (1/1+1) because they are the same
             # species, but we have a single energy spectrum for both T-T
             # neutrons, so we multiply again by 2
             n1_n2 = density_t * density_t
