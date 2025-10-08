29ce0d0337e105552e69425585607c12269cb7cf
CoronelBuendia <matti.coleman@gmail.com>
2025-10-08 10:07:55 +0200

    Add test for JETTO EQDSK and fix sqrt(-0.0) bug (#19)
    * start test and mod example
    
    * fix bug and change example
    
    * fix tests
    
    * ruff

--- tests/test_flux.py
+++ tests/test_flux.py
@@ -11,6 +11,7 @@ from tokamak_neutron_source.error import FluxSurfaceError
 from tokamak_neutron_source.flux import (
     ClosedFluxSurface,
     FausserFluxSurface,
+    FluxConvention,
     FluxMap,
     LCFSInformation,
 )
@@ -137,10 +138,15 @@ TEST_DATA = Path(__file__).parent / "test_data"
     params=[
         Path(TEST_DATA, "DN-DEMO_eqref.json").as_posix(),
         Path(TEST_DATA, "eqref_OOB.json").as_posix(),
+        Path(TEST_DATA, "jetto_600_100000.eqdsk").as_posix(),
     ],
 )
 def flux_map(request):
-    return FluxMap.from_eqdsk(request.param)
+    if request.param.startswith("jetto"):
+        convention = FluxConvention.SQRT
+    else:
+        convention = FluxConvention.LINEAR
+    return FluxMap.from_eqdsk(request.param, flux_convention=convention)
 
 
 @pytest.mark.usefixtures("flux_map")
