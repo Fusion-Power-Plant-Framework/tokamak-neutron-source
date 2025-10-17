6ce047a7264772aeb27ca01c27e44dfccc99c474
ocean <OceanWongUK@gmail.com>
2025-10-17 21:24:10 +0100

    Fixed the test and moved assert statements into separate tests for ease of accounting.

--- tests/test_mcnp_interface.py
+++ tests/test_mcnp_interface.py
@@ -5,14 +5,12 @@
 
 import re
 from dataclasses import dataclass
-from pathlib import Path
 
 import numpy as np
 import pytest
 from numpy import typing as npt
 
 from tokamak_neutron_source import (
-    FluxConvention,
     FluxMap,
     FractionalFuelComposition,
     TokamakNeutronSource,
@@ -22,91 +20,111 @@ from tokamak_neutron_source.profile import ParabolicPedestalProfile
 from tokamak_neutron_source.reactions import Reactions
 
 
-def make_source(source_name: str):
-    match source_name:
-        case "STEP_jetto":
-            return TokamakNeutronSource(
-                transport=TransportInformation.from_jetto(
-                    "tests/test_data/STEP_jetto.jsp"
-                ),
-                flux_map=FluxMap.from_eqdsk(
-                    "tests/test_data/STEP_jetto.eqdsk_out",
-                    flux_convention=FluxConvention.SQRT,
-                ),
-                cell_side_length=0.3,
-            )
-        case "eqref_OOB":
-            return TokamakNeutronSource(
-                transport=TransportInformation.from_parameterisations(
-                    ion_temperature_profile=ParabolicPedestalProfile(
-                        25.0, 5.0, 0.1, 1.45, 2.0, 0.95
-                    ),
-                    fuel_density_profile=ParabolicPedestalProfile(
-                        0.8e20, 0.5e19, 0.5e17, 1.0, 2.0, 0.95
-                    ),
-                    rho_profile=np.linspace(0, 1, 30),
-                    fuel_composition=FractionalFuelComposition(D=0.5, T=0.5),
-                ),
-                flux_map=FluxMap.from_eqdsk("tests/test_data/eqref_OOB.json"),
-                cell_side_length=0.3,
-            )
+@pytest.fixture(scope="module", autouse=True)
+def sdef_path(tmp_path_factory):
+    return tmp_path_factory.mktemp("sdef_output")
+
+
+@pytest.fixture(scope="module")
+def make_source():
+    return TokamakNeutronSource(
+        transport=TransportInformation.from_parameterisations(
+            ion_temperature_profile=ParabolicPedestalProfile(
+                25.0, 5.0, 0.1, 1.45, 2.0, 0.95
+            ),
+            fuel_density_profile=ParabolicPedestalProfile(
+                0.8e20, 0.5e19, 0.5e17, 1.0, 2.0, 0.95
+            ),
+            rho_profile=np.linspace(0, 1, 30),
+            fuel_composition=FractionalFuelComposition(D=0.5, T=0.5),
+        ),
+        flux_map=FluxMap.from_eqdsk("tests/test_data/eqref_OOB.json"),
+        cell_side_length=0.3,
+    )
 
 
 SP_4_SCHEME = {Reactions.D_T: -1, Reactions.D_D: -2}
 
 
-@pytest.mark.parametrize("source_name", ["STEP_jetto", "eqref_OOB"])
-def test_sdef(source_name: str, tmp_path: Path) -> dict[str, str]:
-    source = make_source(source_name)
-    source.to_sdef_card(tmp_path / "test_sdef")
-
+@pytest.fixture(scope="module")
+def make_sdef(make_source, sdef_path):
+    source = make_source
+    source.to_sdef_card(sdef_path / "test_sdef")
+    sdef_dict = {}
     for reaction in source.source_type:
         if reaction not in Reactions:
             continue
         reactant = re.findall(r"[DT]", reaction.label)
         local_file_name = "test_sdef." + reactant[0] + reactant[1]
-        with open(tmp_path / local_file_name) as sdef_file:
-            sdef_text = sdef_file.readlines()
+        with open(sdef_path / local_file_name) as sdef_file:
+            sdef_dict[reaction] = sdef_file.readlines()
+    return sdef_dict, source.num_neutrons_per_second
 
-        # Test the "Total source neutrons:" line
-        source_neutrons_line, sdef_text = scroll_and_get_next_data_line(
-            sdef_text, "C Total source neutrons:"
+
+@pytest.fixture(scope="module", params=[Reactions.D_D, Reactions.D_T, Reactions.T_T])
+def find_sdef_file(make_sdef, request) -> dict[str, str]:
+    sdef_dict, num_neutrons_per_second = make_sdef
+    return (
+        sdef_dict[request.param],
+        num_neutrons_per_second[request.param],
+        request.param,
+    )
+
+
+def test_num_reactions(find_sdef_file):
+    sdef_text, num_neutrons_per_second, _ = find_sdef_file
+
+    # Test the "Total source neutrons:" line
+    source_neutrons_line, _ = scroll_and_get_next_data_line(
+        sdef_text, "C Total source neutrons:"
+    )
+    num_neutrons = float(source_neutrons_line[:-4].split()[-1])
+    assert np.isclose(num_neutrons, num_neutrons_per_second, atol=0, rtol=1e-6), (
+        "Matching number of neutrons"
+    )
+
+
+def test_sdef_line(find_sdef_file):
+    sdef_text, _, _ = find_sdef_file
+
+    # Test sdef
+    sdef, _ = scroll_and_get_next_data_line(sdef_text, "sdef")
+    assert bool(sdef), "SDEF file line must exist"
+
+
+def test_radial_dist(find_sdef_file):
+    sdef_text, _, reaction = find_sdef_file
+
+    # Test neutron energy distribution
+    if reaction is Reactions.T_T:
+        _, sp, sdef_text = get_next_si_and_sp(sdef_text, 2)
+    else:
+        energy_dist_str, sdef_text = scroll_and_get_next_data_line(sdef_text, "SP2")
+        sp = tokenize(energy_dist_str)
+        assert int(sp.dist_type) == -4, "-4 for fusion neutron source of DD/DT type."
+        ion_temp_mev = sp.data[0]
+        assert 1e-3 <= ion_temp_mev <= 0.1, (
+            "Weighted ion temperature expected to be between 1 to 100 keV."
         )
-        num_neutrons = float(source_neutrons_line[:-4].split()[-1])
-        assert np.isclose(
-            num_neutrons, source.num_neutrons_per_second[reaction], atol=0, rtol=1e-6
-        ), "Matching number of neutrons"
+        assert sp.data[-1] == SP_4_SCHEME[reaction], "-1 for DT, -2 for DD."
+    assert sp.def_number == 2
 
-        # Test sdef
-        sdef, sdef_text = scroll_and_get_next_data_line(sdef_text, "sdef")
-        assert bool(sdef), "SDEF file line must exist"
+    # Test radial distribution
+    _, sp, sdef_text = get_next_si_and_sp(sdef_text, 3)
 
-        # Test neutron energy distribution
-        if local_file_name.endswith("TT"):
-            _, sp, sdef_text = get_next_si_and_sp(sdef_text, 2)
-        else:
-            energy_dist_str, sdef_text = scroll_and_get_next_data_line(sdef_text, "SP2")
-            sp = tokenize(energy_dist_str)
-            assert int(sp.dist_type) == -4, "-4 for fusion neutron source of DD/DT type."
-            ion_temp_mev = sp.data[0]
-            assert 1e-3 <= ion_temp_mev <= 0.1, (
-                "Between 1 to 100 keV of ion-temp expected."
-            )
-            assert sp.data[-1] == SP_4_SCHEME[reaction], "-1 for DT, -2 for DD."
-        assert sp.def_number == 2
 
-        # Test radial distribution
-        _, sp, sdef_text = get_next_si_and_sp(sdef_text, 3)
+def test_DS4_and_vertical_dists(find_sdef_file):
+    sdef_text, _, _ = find_sdef_file
 
-        # get list of vertical distributions
-        ds4, sdef_text = scroll_and_get_next_data_line(sdef_text, "DS4")
-        ds4 = tokenize(ds4, dtype=int)
-        assert ds4.dist_type == "S"
-        for i in ds4.data:
-            _, sp, sdef_text = get_next_si_and_sp(sdef_text, i)
+    # get list of vertical distributions
+    ds4, sdef_text = scroll_and_get_next_data_line(sdef_text, "DS4")
+    ds4 = tokenize(ds4, dtype=int)
+    assert ds4.dist_type == "S", "defined by cell"
+    for i in ds4.data:
+        _, _, sdef_text = get_next_si_and_sp(sdef_text, i)
 
-        # Test end of file
-        assert not scroll_and_get_next_data_line(sdef_text, "SP")[0], "EoF expected"
+    # Test end of file
+    assert not scroll_and_get_next_data_line(sdef_text, "SP")[0], "EoF expected"
 
 
 def scroll_and_get_next_data_line(
