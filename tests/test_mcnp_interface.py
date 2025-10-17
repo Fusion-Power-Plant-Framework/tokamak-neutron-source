aba8d237e6e5b19ae33acc7e3fe37ddc4d97bc7f
ocean <OceanWongUK@gmail.com>
2025-10-17 19:52:37 +0100

    ruff fixes.

--- tests/test_mcnp_interface.py
+++ tests/test_mcnp_interface.py
@@ -1,36 +1,42 @@
-import pytest
+# SPDX-FileCopyrightText: 2024-present Tokamak Neutron Source Maintainers
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+"""MCNP neutron source (SDEF) interface tester"""
+
 import re
-import numpy as np
-from numpy import typing as npt
-from pathlib import Path
 from dataclasses import dataclass
+from pathlib import Path
+
+import numpy as np
+import pytest
+from numpy import typing as npt
 
 from tokamak_neutron_source import (
     FluxConvention,
-    FractionalFuelComposition,
     FluxMap,
+    FractionalFuelComposition,
     TokamakNeutronSource,
     TransportInformation,
 )
-from tokamak_neutron_source.reactions import Reactions
 from tokamak_neutron_source.profile import ParabolicPedestalProfile
+from tokamak_neutron_source.reactions import Reactions
+
 
 def make_source(source_name: str):
     match source_name:
         case "STEP_jetto":
-            jetto_source = TokamakNeutronSource(
+            return TokamakNeutronSource(
                 transport=TransportInformation.from_jetto(
                     "tests/test_data/STEP_jetto.jsp"
                 ),
                 flux_map=FluxMap.from_eqdsk(
                     "tests/test_data/STEP_jetto.eqdsk_out",
-                    flux_convention=FluxConvention.SQRT
+                    flux_convention=FluxConvention.SQRT,
                 ),
                 cell_side_length=0.3,
             )
-            return jetto_source
         case "eqref_OOB":
-            eqdsk_source = TokamakNeutronSource(
+            return TokamakNeutronSource(
                 transport=TransportInformation.from_parameterisations(
                     ion_temperature_profile=ParabolicPedestalProfile(
                         25.0, 5.0, 0.1, 1.45, 2.0, 0.95
@@ -44,11 +50,12 @@ def make_source(source_name: str):
                 flux_map=FluxMap.from_eqdsk("tests/test_data/eqref_OOB.json"),
                 cell_side_length=0.3,
             )
-            return eqdsk_source
+
 
 SP_4_SCHEME = {Reactions.D_T: -1, Reactions.D_D: -2}
 
-@pytest.mark.parametrize(("source_name"), ("STEP_jetto", "eqref_OOB"))
+
+@pytest.mark.parametrize("source_name", ["STEP_jetto", "eqref_OOB"])
 def test_sdef(source_name: str, tmp_path: Path) -> dict[str, str]:
     source = make_source(source_name)
     source.to_sdef_card(tmp_path / "test_sdef")
@@ -57,18 +64,17 @@ def test_sdef(source_name: str, tmp_path: Path) -> dict[str, str]:
         if reaction not in Reactions:
             continue
         reactant = re.findall(r"[DT]", reaction.label)
-        local_file_name = "test_sdef."+ reactant[0]+reactant[1]
+        local_file_name = "test_sdef." + reactant[0] + reactant[1]
         with open(tmp_path / local_file_name) as sdef_file:
             sdef_text = sdef_file.readlines()
 
         # Test the "Total source neutrons:" line
         source_neutrons_line, sdef_text = scroll_and_get_next_data_line(
-            sdef_text,
-            "C Total source neutrons:"
+            sdef_text, "C Total source neutrons:"
         )
         num_neutrons = float(source_neutrons_line[:-4].split()[-1])
         assert np.isclose(
-            num_neutrons, source.num_neutrons_per_second[reaction], atol=0, rtol=1E-6
+            num_neutrons, source.num_neutrons_per_second[reaction], atol=0, rtol=1e-6
         ), "Matching number of neutrons"
 
         # Test sdef
@@ -77,28 +83,31 @@ def test_sdef(source_name: str, tmp_path: Path) -> dict[str, str]:
 
         # Test neutron energy distribution
         if local_file_name.endswith("TT"):
-            si, sp, sdef_text = get_next_si_and_sp(sdef_text, 2)
+            _, sp, sdef_text = get_next_si_and_sp(sdef_text, 2)
         else:
             energy_dist_str, sdef_text = scroll_and_get_next_data_line(sdef_text, "SP2")
             sp = tokenize(energy_dist_str)
             assert int(sp.dist_type) == -4, "-4 for fusion neutron source of DD/DT type."
             ion_temp_mev = sp.data[0]
-            assert 1E-3<=ion_temp_mev<=0.1, "Between 1 to 100 keV of ion-temp expected."
+            assert 1e-3 <= ion_temp_mev <= 0.1, (
+                "Between 1 to 100 keV of ion-temp expected."
+            )
             assert sp.data[-1] == SP_4_SCHEME[reaction], "-1 for DT, -2 for DD."
         assert sp.def_number == 2
 
         # Test radial distribution
-        si, sp, sdef_text = get_next_si_and_sp(sdef_text, 3)
-        
+        _, sp, sdef_text = get_next_si_and_sp(sdef_text, 3)
+
         # get list of vertical distributions
         ds4, sdef_text = scroll_and_get_next_data_line(sdef_text, "DS4")
         ds4 = tokenize(ds4, dtype=int)
         assert ds4.dist_type == "S"
         for i in ds4.data:
-            si, sp, sdef_text = get_next_si_and_sp(sdef_text, i)
+            _, sp, sdef_text = get_next_si_and_sp(sdef_text, i)
 
-        # Test end of file 
-        assert scroll_and_get_next_data_line(sdef_text, "SP")[0] == "", "EoF expected"
+        # Test end of file
+        assert not scroll_and_get_next_data_line(sdef_text, "SP")[0], "EoF expected"
+
 
 def scroll_and_get_next_data_line(
     text_list: list[str], match_string: str
@@ -106,15 +115,22 @@ def scroll_and_get_next_data_line(
     """
     Search the content of the file until the a row matching the match_string appears.
     Then, split just before this matched line.
+
+    Returns
+    -------
+    :
+        The matching data line, shrunken into a single string.
+    text_list:
+        the remaining text after that line, returned as a list of strings.
     """
     for i, line in enumerate(text_list):
         if line.startswith(match_string):
             j = i + 1
-            while j<len(text_list) and text_list[j].startswith(" "):
+            while j < len(text_list) and text_list[j].startswith(" "):
                 j += 1
             return " ".join(line.lstrip() for line in text_list[i:j]), text_list[j:]
-    else:
-        return "", []
+    return "", []
+
 
 MNEMONIC_TRANSLATION_DICT = {
     "SI": "source information",
@@ -126,9 +142,12 @@ MNEMONIC_EXPLANATION_DICT = {
     "SP": "y-axis of a probability density function",
     "DS": "dependent source",
 }
+
+
 @dataclass
 class MCNPDefinitionLine:
     """Data provided by one line in an MCNP input file."""
+
     mnemonic: str
     def_number: int
     dist_type: str | int
@@ -142,26 +161,52 @@ class MCNPDefinitionLine:
     def explanation(self):
         return MNEMONIC_EXPLANATION_DICT[self.mnemonic]
 
+
 def tokenize(line: str, dtype=float):
-    """Parse a line of MCNP input file."""
+    """Parse a line of MCNP input file.
+
+    Returns
+    -------
+    :
+        An instance of MCNPDefinitionLine
+    """
     data = line.split()
     line_type, dist_num = data[0][:2], int(data[0][2:])
     if "." in data[1]:
         return MCNPDefinitionLine(
-            line_type, dist_num, None,
+            line_type,
+            dist_num,
+            None,
             np.array(data[1:], dtype=dtype),
         )
     return MCNPDefinitionLine(
-        line_type, dist_num, data[1],
+        line_type,
+        dist_num,
+        data[1],
         np.array(data[2:], dtype=dtype),
     )
 
-def get_next_si_and_sp(text_list: list[str], i:int) -> tuple[MCNPDefinitionLine, MCNPDefinitionLine, list[str]]:
+
+def get_next_si_and_sp(
+    text_list: list[str], i: int
+) -> tuple[MCNPDefinitionLine, MCNPDefinitionLine, list[str]]:
+    """
+    Get the matching SIn and SPn lines, and the rest of the SDEF file below that.
+
+    Returns
+    -------
+    si:
+        The SIn line.
+    sp:
+        The SPn line.
+    remaining_text_list:
+        text, returned as a list.
+    """
     si, remaining_texts = scroll_and_get_next_data_line(text_list, f"SI{i}")
     sp, remaining_texts = scroll_and_get_next_data_line(remaining_texts, f"SP{i}")
     si, sp = tokenize(si), tokenize(sp)
-    assert (si.dist_type, sp.dist_type) in (("H", "D"), ("A", None)), (
+    assert (si.dist_type, sp.dist_type) in set(("H", "D"), ("A", None)), (
         "Either histogramic or tabular distribution expected."
     )
-    assert len(si.data)==len(sp.data), "Equal number of x- and y-values expected."
+    assert len(si.data) == len(sp.data), "Equal number of x- and y-values expected."
     return si, sp, remaining_texts
